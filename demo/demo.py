76ae2088e00f425698bd223a356df24251af38f4
grothesk <malte.groth@gmx.net>
2019-01-12 02:20:48 +0100

    demo.py: automate use of docker container

--- demo/demo.py
+++ demo/demo.py
@@ -1,8 +1,32 @@
+import os
+import time
+import docker
+
 from noobSQL.utils.db import Connection, Naming
 from noobSQL.bi.bio import BIResponder
 
 
+HERE = os.path.dirname(__file__)
+
+
 if __name__ == '__main__':
+    # Initialize docker client
+    cli = docker.from_env()
+
+    # Build a docker image
+    cli.images.build(path=HERE, tag='docker_pg')
+
+    # Create and run a container
+    container = cli.containers.run(
+        image='docker_pg',
+        detach=True,
+        ports={5432: 5432}
+    )
+
+    # Wait to make sure that the container is up and running
+    time.sleep(5) # todo: There must be a better way...
+
+    # Run noobSQL demo
     file = 'demo.yml'
     connection = Connection.from_yaml(file)
     naming = Naming.from_yaml(file)
@@ -12,3 +36,6 @@ if __name__ == '__main__':
 
     print(query)
     print(dau)
+
+    # Kill the container
+    container.kill()
